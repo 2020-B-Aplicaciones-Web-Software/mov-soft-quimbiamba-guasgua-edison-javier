482e20dcf83a6bf6810a9593ae6e4f515f2eccb7
Edison Quimbiamba <edison.quimbiamba@epn.edu.ec>
2021-09-26 11:12:00 -0500

    Creación de interfaces de navegación

---
+++ Examen02/app/src/main/java/com/example/examen02/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.examen02
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val btnIrModuloMateriaActivity =
+            findViewById<Button>(R.id.btn_materias)// /ir  al modulo de materias
+        btnIrModuloMateriaActivity.setOnClickListener {
+            abrirActividad(MateriaActivity::class.java)
+        }
+
+        val btnIrModuloEstudianteActivity = findViewById<Button>(R.id.btn_estudiante)
+        btnIrModuloEstudianteActivity.setOnClickListener {
+            abrirActividad(EstudianteActivity::class.java)
+        }
+
+    }
+    private fun abrirActividad(clase: Class<*>) {
+        val intentExplicito = Intent(this, clase) //this menciona la página actual
+        this.startActivity(intentExplicito)
+    }
+
+
+
+
+
+}
