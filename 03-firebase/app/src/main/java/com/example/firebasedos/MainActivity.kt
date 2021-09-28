2b22cf84e1269fcb78f60cc2b93cd027fce27d58
Edison Quimbiamba <edison.quimbiamba@epn.edu.ec>
2021-09-27 23:37:03 -0500

    Creación de  formulario :D

--- 03-firebase/app/src/main/java/com/example/firebasedos/MainActivity.kt
+++ 03-firebase/app/src/main/java/com/example/firebasedos/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.firebasedos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.firebase.ui.auth.AuthUI
+import com.google.android.gms.auth.api.Auth
 
 class MainActivity : AppCompatActivity() {
+    val CODIGO_INICIO_SESION = 102
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val botonLogin = findViewById<Button>(R.id.btn_login)
+        botonLogin.setOnClickListener {
+            llamarLoginUsuario()
+        }
+    }
+    fun llamarLoginUsuario(){
+        val providers = arrayListOf(
+            //LISTA  DE PROVEEDORES
+            AuthUI.IdpConfig.EmailBuilder().build()
+        )
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .setTosAndPrivacyPolicyUrls(
+                 "https://example.com/terms.html",
+                "https://example.com/privacy.html"
+                ).build(),
+            CODIGO_INICIO_SESION
+            )
     }
 }
