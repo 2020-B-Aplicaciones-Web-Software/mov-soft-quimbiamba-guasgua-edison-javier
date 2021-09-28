d959032af032e24fb016223b1996d689f9774641
Edison Quimbiamba <edison.quimbiamba@epn.edu.ec>
2021-09-28 12:37:27 -0500

    Informe 29 -- Registro de usuarios

--- 03-firebase/app/src/main/java/com/example/firebasedos/MainActivity.kt
+++ 03-firebase/app/src/main/java/com/example/firebasedos/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.example.firebasedos
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
 import com.google.android.gms.auth.api.Auth
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     val CODIGO_INICIO_SESION = 102
@@ -16,7 +23,8 @@ class MainActivity : AppCompatActivity() {
             llamarLoginUsuario()
         }
     }
-    fun llamarLoginUsuario(){
+
+    fun llamarLoginUsuario() {
         val providers = arrayListOf(
             //LISTA  DE PROVEEDORES
             AuthUI.IdpConfig.EmailBuilder().build()
@@ -26,10 +34,61 @@ class MainActivity : AppCompatActivity() {
                 .createSignInIntentBuilder()
                 .setAvailableProviders(providers)
                 .setTosAndPrivacyPolicyUrls(
-                 "https://example.com/terms.html",
-                "https://example.com/privacy.html"
+                    "https://example.com/terms.html",
+                    "https://example.com/privacy.html"
                 ).build(),
             CODIGO_INICIO_SESION
-            )
+        )
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            CODIGO_INICIO_SESION -> {
+                if (resultCode == Activity.RESULT_OK) {
+                    val usuario: IdpResponse? = IdpResponse.fromResultIntent(data)
+
+                    if (usuario != null) {
+                        if (usuario.isNewUser == true) {
+                            Log.i("firebase-login", "Nuevo usuario")
+                            registrarUsuarioPorPrimeraVez(usuario)
+                        } else {
+
+                            Log.i("firebase-login", "Usuario antiguo")
+                        }
+                    } else {
+                        Log.i("firebase-login", "El usuario cancelo")
+                    }
+                }
+            }
+        }
+
+    }
+
+    fun registrarUsuarioPorPrimeraVez(usuario: IdpResponse) {
+        val usuarioLogeado = FirebaseAuth
+            .getInstance()
+            .currentUser
+        if (usuario.email != null && usuarioLogeado != null) {
+            //roles: ["usuario", "Admin"]
+            // uid
+            val db = Firebase.firestore//obtenemos  la referencia
+            val rolesUsuario = arrayListOf("usuario") // creamos el arreglo de permisos
+            val nuevoUsuario = hashMapOf<String, Any>(
+                "roles" to rolesUsuario,
+                "uid" to usuarioLogeado.uid
+            )
+
+            db.collection("usuario")
+                .add(nuevoUsuario)
+                .addOnSuccessListener {
+                    Log.i("fire-firestore", "Se creo")
+                }
+                .addOnFailureListener {
+                    Log.i("firebase-firestore", "fallo")
+                }
+        }
+    }
+
+
 }
